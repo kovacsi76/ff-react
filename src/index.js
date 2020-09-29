9a7bdae389a57ba951233cbe0bfb8937a869e82a
Istvan Kovacs <istvan.kovacs@pureretirement.co.uk>
2020-09-29 09:50:46 +0100

    Adding node-sass to project.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './App.css';
+import './App.scss';
 import { FactFind } from './containers/FactFind';
 import * as serviceWorker from './serviceWorker';
 
